641a6b8d4c236025b6e5646cb2a3fbe617b41736
Nadia Kausar <kausarnadia32@gmail.com>
2025-07-19 16:25:44 -0700

    PORTFOLIO:
    $ git commit -m PORTFOLio

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css'; // Optional if you're using styles
+import App from './App';
+import './index.css'; // or App.css
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
